40da1239b962d5da5eb4367e588ab4b028b38c60
Nikita Novikov <monstars51@gmail.com>
2023-04-06 21:25:20 +0300

    one

---
+++ src/main/java/test/java/MedicalServiceImplTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+public class MedicalServiceImplTest {
+    //Написать тесты для проверки класса MedicalServiceImpl, сделав заглушку для класса PatientInfoFileRepository,
+    //который он использует
+    //Проверить вывод сообщения во время проверки давления checkBloodPressure
+    //Проверить вывод сообщения во время проверки температуры checkTemperature
+    //Проверить, что сообщения не выводятся, когда показатели в норме.
+
+    //Создайте тесты в соответствии с задачей (для сервиса MedicalServiceImpl нужно
+    // обязательно создать заглушки (mock) в виде PatientInfoFileRepository и SendAlertService)
+    // - сделать минимум 3 unit-теста;
+    //Для заглушки класса SendAlertService нужно проверить вызов метода send
+    // (можно воспользоваться ArgumentCaptor и verify из библиотеки mockito)
+}
